1a45935f3901eba7fed6d835c6665a0ec0f9d8fe
DaikoneKisu <eduardoarzolay@gmail.com>
2025-05-27 10:20:14 -0400

    feat: add interactive pairplot with selectbox and add linear regression model

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit
 import matplotlib.pyplot as plt
 import seaborn
 import numpy
+import pandas
+from sklearn.model_selection import train_test_split
 
 color = "#83c9ff"
 
@@ -9,7 +11,28 @@ color = "#83c9ff"
 def get_penguins():
     return seaborn.load_dataset("penguins")
 
+@streamlit.cache_resource
+def get_linear_regression(x_train, y_train):
+    from sklearn.linear_model import LinearRegression
+
+    model = LinearRegression()
+
+    return model.fit(x_train, y_train)
+
 dataset = get_penguins()
+
+dataset = dataset.dropna()
+
+feature_columns = dataset.columns.difference(pandas.Index(["body_mass_g", "island", "sex", "species"]))
+
+x = dataset[feature_columns]
+y = dataset[["body_mass_g"]]
+    
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+model = get_linear_regression(x_train, y_train)
+
+y_predict = model.predict(x_test)
 
 streamlit.title("Ejemplo con Streamlit: Dataset de pingüinos")
 
@@ -74,3 +97,24 @@ correlation_figure
 
 streamlit.subheader("Gráfico de pares de las variables numéricas")
 streamlit.pyplot(seaborn.pairplot(dataset, hue="species"))
+
+specie = streamlit.selectbox(
+    "Selecciona una especie de pingüino para ver su distribución",
+    options=dataset["species"].unique(),
+    index=0
+)
+
+streamlit.subheader(f"Distribución de la especie {specie}")
+streamlit.pyplot(seaborn.pairplot(dataset[dataset["species"] == specie], hue="species"))
+
+fig, axes = plt.subplots(len(x_test.columns.values), 1, sharey=True, constrained_layout=True, figsize=(30,15))
+fig.legend()
+
+for i, e in enumerate(x_test.columns):
+  axes[i].set_title("Mejor linea de ajuste")
+  axes[i].set_xlabel(str(e))
+  axes[i].set_ylabel('exam_score')
+  axes[i].scatter(x_test[e].values[:,numpy.newaxis], y_test, color='blue', label='Real')
+  axes[i].scatter(x_test[e].values[:,numpy.newaxis], y_predict, color='red', label='Predicción')
+
+fig
